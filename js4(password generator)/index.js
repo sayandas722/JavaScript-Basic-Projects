175034e2d2cc04c01616359c9849e8a98a50d03e
SAYAN DAS <73294479+sayandas722@users.noreply.github.com>
2024-01-08 19:51:05 +0530

    Add files via upload

---
+++ js4(password generator)/index.js
@@ -0,0 +1,33 @@
+//  let concEl=document.getElementById("passEl");
+
+function generatepass() {
+  class Password {
+    constructor() {
+    //   console.log("Welcome to Password Generator");
+      this.pass = "";
+    }
+    generatePassword(len) {
+      let chars = "abcdefghijklmnopqrstuvwxyz";
+      let numbers = "1234567890";
+      let special = "!@#$%^&*()";
+      if (len < 3) {
+        console.log("Your password should be atleast 3 characters long");
+      } else {
+        let i = 0;
+        while (i < len) {
+          this.pass += chars[Math.floor(Math.random() * chars.length)];
+          this.pass += numbers[Math.floor(Math.random() * numbers.length)];
+          this.pass += special[Math.floor(Math.random() * special.length)];
+          i += 3;
+        }
+        this.pass = this.pass.substring(0, len);
+        return this.pass;
+      }
+    }
+  }
+
+  let p = new Password();
+//  const pp=console.log(p.generatePassword(7));
+ alert(console.log(p.generatePassword(7)));
+   
+}
